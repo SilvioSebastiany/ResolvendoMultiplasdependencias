55a27638e3a658e65ce22b438f636131c952bba9
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2025-04-28 11:35:26 -0300

    TryAddTransient

--- Program.cs
+++ Program.cs
@@ -2,11 +2,16 @@ using DependencyInjectionLifetimeSample.Services;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddTransient<IService, PrimaryService>();
+builder.Services.TryAddTransient<IService, SecondaryService>();
+builder.Services.TryAddTransient<IService, PrimaryService>();
+builder.Services.TryAddTransient<IService, PrimaryService>();
+builder.Services.TryAddTransient<IService, SecondaryService>();
+//builder.Services.AddTransient<IService, TertiaryService>();
 
 var app = builder.Build();
 
-app.MapGet("/", (IService s) => Results.Ok(s.GetType().Name));
+app.MapGet("/", (IEnumerable<IService> services) 
+    => Results.Ok(services.Select(s => s.GetType().Name)));
 
 app.Run();
 
